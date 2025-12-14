228ed938e3ec69818a4bf91240c08cec4479e886
riddhishasolutions <riddhishasolutions@gmail.com>
2025-12-14 10:40:03 +0530

    Delete assets directory

--- assets/js/scroll.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('scroll',()=>{
-document.querySelectorAll('.fade').forEach(el=>{
-if(el.getBoundingClientRect().top < window.innerHeight-120){
-el.classList.add('show')
-}})
-})
